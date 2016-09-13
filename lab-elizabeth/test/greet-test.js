ac88b99e8ce4f0f1e31132aabe4dc6113ec850d4
Elizabeth Kleinschmidt <tenlia@hotmail.com>
2016-09-13 16:10:26 -0700

    testing something on my greet-test

--- lab-elizabeth/test/greet-test.js
+++ lab-elizabeth/test/greet-test.js
@@ -5,8 +5,8 @@ const assert = require('assert');
 
 describe('testing greet.js module', function(){
   it('should return hello lizzie', function(){
-    var result = greet('lizzie');
-    assert.equal(result, 'hello lizzie', 'expected "hello lizzie"');
+    // var result = greet('lizzie');
+    assert.equal(greet('lizzie'), 'hello lizzie', 'expected "hello lizzie"');
   });
   it('should throw error for missing input', function(){
     assert.throws(function(){
