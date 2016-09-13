8980fdb615868c55d963b7f47059c08410f8beb3
Elizabeth Kleinschmidt <tenlia@hotmail.com>
2016-09-13 14:24:40 -0700

    figuring-out the testing code bit-by-bit

--- lab-elizabeth/test/test-greet.js
+++ lab-elizabeth/test/test-greet.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const greet = require('../lib/greet');
+const assert = require('assert');
+
+describe('testing greet.js module', function(){
+  it('should return hello liz', function(){
+    var result = greet('liz');
+    assert.equal(result, 'hello liz', 'expected "hello liz"');
+  });
+});
